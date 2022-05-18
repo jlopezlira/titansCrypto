3a0a460952b74c7913aa7e7421b5ad3e75030f7c
Juan José López Lira <jlopezlira@gmail.com>
2022-05-17 21:48:20 -0500

    Initial Setup

--- App.tsx
+++ App.tsx
@@ -1,12 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
 export default function App() {
+  const SharedValues = () => {
+    const randomWidth = useSharedValue(10);
+
+    const config = {
+      duration: 500,
+      easing: Easing.bezier(0.25, 0.1, 0.25, 1),
+    };
+
+    const myStyle = useAnimatedStyle(() => {
+      return {
+        width: withTiming(randomWidth.value, config),
+        height: 20,
+        backgroundColor: 'green',
+      };
+    });
+
+    return (
+      <>
+        <Text>Shared Values</Text>
+        <Animated.View style={myStyle} />
+        <Button title="Change" onPress={() => (randomWidth.value = Math.random() * 350)} />
+      </>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <SharedValues />
+      </View>
+    </NavigationContainer>
   );
 }
 
@@ -14,7 +46,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
